bc0d8c797b7b9fb30a77bfd48821cd9e1cc91893
ximena campos <xa.camposmiranda@ugto.mx>
2026-05-04 15:59:59 -0600

    [CREATED]: se creo la base y visualización del QR

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -14,7 +14,7 @@
   };
 
   const app = initializeApp(firebaseConfig);
-  const auth=getAuth(app);
-  const db=getFirestore(app)
+  const auth = getAuth(app);
+  const db = getFirestore(app)
 
   export {app, auth, db}

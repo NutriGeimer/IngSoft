626dccd8c186bc04b8bb9a87aa4d6df00c248041
TheBlackMan2099 <luismujica2099@hotmail.com>
2026-05-04 22:16:09 -0600

    [MODIFIED]: Se cambia variable relacionada a ciudad favorita a numero de placas, asi como la vinculacion a la BDD de firebase y el html de reigister

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,12 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-  import {getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-
+  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyDRAXSZNc1WtyILldwPLX97Pm9jfGDNPPg",
     authDomain: "beepark-42a9b.firebaseapp.com",
@@ -13,8 +16,10 @@
     appId: "1:528130557502:web:2b1bcefe75903d6a2ab36f"
   };
 
-  const app = initializeApp(firebaseConfig);
-  const auth=getAuth(app);
-  const db=getFirestore(app)
+  // Initialize Firebase
 
-  export {app, auth, db}
+const app=initializeApp(firebaseConfig);
+const auth= getAuth(app);
+const db=getFirestore(app);
+
+export {app,auth, db}

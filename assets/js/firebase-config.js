975e04b889484fa7aec7db0b03a5a9e4a3f46549
xecsarin <c.servingonzalez@ugto.mx>
2026-05-09 02:26:02 -0600

    [UPDATED]: Se migra firebase-config al proyecto beeparking-a33a4

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,25 +1,18 @@
- // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyCCOC2CKdPaoxP-k3z5Tto9gtKCaFUn8Ko",
+  authDomain: "beeparking-a33a4.firebaseapp.com",
+  projectId: "beeparking-a33a4",
+  storageBucket: "beeparking-a33a4.firebasestorage.app",
+  messagingSenderId: "577983473721",
+  appId: "1:577983473721:web:5df7348b8545b3361a05e8"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDRAXSZNc1WtyILldwPLX97Pm9jfGDNPPg",
-    authDomain: "beepark-42a9b.firebaseapp.com",
-    projectId: "beepark-42a9b",
-    storageBucket: "beepark-42a9b.firebasestorage.app",
-    messagingSenderId: "528130557502",
-    appId: "1:528130557502:web:2b1bcefe75903d6a2ab36f"
-  };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-
-
-const app=initializeApp(firebaseConfig);
-const auth= getAuth(app);
-const db=getFirestore(app);
-
-export {app,auth, db}
+export { app, auth, db };

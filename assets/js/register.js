f98f1a2f520e0217ea4a006c2035278063832ca3
ximena campos <xa.camposmiranda@ugto.mx>
2026-05-19 20:08:23 -0600

    [ADDED]: añadimos validación para que no se repita la placa

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,6 @@
 import { hideAlert, showAlert, setButtonLoading, registerUser, getFirebaseErrorMessage } from "./auth.js"
+import { db } from "./firebase-config.js";
+import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
 const form=document.getElementById('registerForm')
 const nameInput=document.getElementById('name')
@@ -13,39 +15,43 @@ form?.addEventListener('submit', async (e)=>{
     e.preventDefault()
    
     hideAlert('registerAlert')
-   // successBox?.classList.add('d-none')
-   // successBox?.textContent=''
-
+  
     const name= nameInput.value.trim() 
     const email= emailInput.value.trim()
     const nplates=plateInput.value.trim() 
     const password= passwordInput.value.trim() 
     const confirmPassword= confirmPasswordInput.value.trim() 
 
-    if(!name || !email || !password || !confirmPassword){
+    if(!name || !email || !password || !confirmPassword || !nplates){
         showAlert('registerAlert', 'Todos los datos son onligatorios')
-            return
+        return
     }
 
-    //Agregar if para contraseña menor a 6 caracteres 
-
+    if (password.length < 6){
+        showAlert('registerAlert', 'La contraseña debe tener más de 6 caracteres')
+        return
+    }
 
     if(password !== confirmPassword){
         showAlert('registerAlert', 'Las contraseñas no son iguales ')   
         return    
     }
 
-  
+    const q = query(collection(db, 'users'), where('nplates', '==', nplates))
+    const placaExistente = await getDocs(q)
+
+    if(!placaExistente.empty){
+        showAlert('registerAlert', 'Esta placa ya está registrada en el sistema')
+        return
+    }
+
 
     try {
         setButtonLoading(registerBtn,true, '<i class="bi bi-person-check me-2"></i> crear cuenta', 
             'creando cuenta'
         )
         await registerUser({name,email, password, nplates})
-       
-      // successBox?.textContent='cuenta creada correctamente'
-      // successBox?.classList.remove('d-none')
-
+ 
        setTimeout(()=>{
         window.location.href='./login.html'
        }, 1200)

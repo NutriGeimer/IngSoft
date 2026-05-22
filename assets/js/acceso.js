7b4a53f2d46985f431fefd75a145cf6ebca55a93
ximena campos <xa.camposmiranda@ugto.mx>
2026-05-21 18:45:28 -0600

    [ADDED]: se hizo merge de la pagina responsive y logica de seleccion de cajon

--- assets/js/acceso.js
+++ assets/js/acceso.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase-config.js";
+import { db, auth } from "./firebase-config.js";
 import {
     doc,
     getDoc,
@@ -28,6 +28,14 @@ async function validarToken() {
         if (docSnap.exists() && docSnap.data().active === true) {
             await updateDoc(docRef, { active: false, usedAt: serverTimestamp() });
             mostrarExito();
+
+            const usuarioActualId = auth.currentUser.uid; 
+            const userRef = doc(db, "users", usuarioActualId); 
+
+            //validar con éxito y guardar hora 
+            await updateDoc(userRef, { 
+                accesoValidadoAt: new Date() 
+            });
         } else {
             mostrarError("Este código ya fue usado o no existe");
         }

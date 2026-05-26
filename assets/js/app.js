55272a0cab2c42afa3982a53f68bef5c2a40193f
ximena campos <xa.camposmiranda@ugto.mx>
2026-05-25 21:21:40 -0600

    [STYLE]: modificamos el estilo de login y register

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,10 @@
 const showAlert=(elementId, message) => {
     const alert=document.getElementById(elementId)
     alert.textContent=message
-    alert.classList.remove('d-none')
+    alert.classList.remove('hidden')
 }
 
 const hideAlert=(elementId) =>{
     const alert=document.getElementById(elementId)
-    alert.classList.add('d-none')
+    alert.classList.add('hidden')
 }
